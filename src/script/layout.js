bdb6ae6dc99565c791beb82f8373e2cfa196f922
Makstorm <mxm.horobets@gmail.com>
2023-09-28 02:48:32 +0300

    done js-layout

---
+++ src/script/layout.js
@@ -0,0 +1,106 @@
+export const createElement = (tag, className, text) => {
+  const elem = document.createElement(tag)
+
+  if (className) {
+    elem.className = className
+  }
+
+  if (text) {
+    elem.innerHTML = text
+  }
+
+  return elem
+}
+
+const HEADER_BUTTON_LIST = [
+  { width: 24, height: 24, src: '/svg/header__back.svg' },
+  { width: 24, height: 24, src: '/img/header__avatar.png' },
+]
+
+export const createHeader = () => {
+  const header = createElement('header', 'header')
+
+  HEADER_BUTTON_LIST.forEach((params) => {
+    const button = createElement('button', 'button')
+
+    const img = createElement('img')
+
+    Object.entries(params).forEach(([key, value]) => {
+      img[key] = value
+    })
+
+    button.append(img)
+
+    header.append(button)
+  })
+
+  return header
+}
+
+const POST_LIST = [
+  {
+    category: [
+      { text: 'Важливо', id: 1 },
+      { text: 'Нова', id: 2 },
+    ],
+    info: 'До біса планувальник, наймаємо дизайнера і готуємося до презентації, як Джобс',
+    date: '25.01',
+    viewed: false,
+  },
+  {
+    category: [{ text: 'Нова', id: 2 }],
+    info: 'До біса планувальник, наймаємо дизайнера і готуємося до презентації, як Джобс',
+    date: '24.01',
+    viewed: true,
+  },
+]
+
+export const createPost = () => {
+  const postList = createElement('main', 'post__list')
+
+  POST_LIST.forEach((postData) => {
+    const post = createElement(
+      'div',
+      postData.viewed
+        ? 'post button post--viewed'
+        : 'post button',
+    )
+
+    const postHeader = createElement('div', 'post__header')
+
+    const categoryList = createElement(
+      'div',
+      'post__category-list',
+    )
+
+    postData.category.forEach((category) => {
+      const categorySpan = createElement(
+        'span',
+        `post__category post__category--${category.id}`,
+        category.text,
+      )
+
+      categoryList.append(categorySpan)
+    })
+
+    const dateSpan = createElement(
+      'span',
+      'post__date',
+      postData.date,
+    )
+
+    postHeader.append(categoryList, dateSpan)
+
+    const infoParagraph = createElement(
+      'p',
+      'post__info',
+      postData.info,
+    )
+
+    post.append(postHeader, infoParagraph)
+
+    postList.append(post)
+  })
+
+  return postList
+}
